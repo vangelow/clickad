294b23c187bd5073e0879b2c95fe9672e2f1aeb7
stela24 <stela.24@abv.bg>
2017-07-10 23:31:28 +0300

    css and js add

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.owl-carousel').owlCarousel({
+        items:1,
+        merge:true,
+        loop:true,
+        margin:10,
+        video:true,
+        center:true
+    })
+})
